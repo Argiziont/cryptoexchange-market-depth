453049c78f611576e9ec616817a287330d1331d0
Andrii mandryk <andreyargiziont@gmail.com>
2024-12-20 16:14:50 +0200

    Added Misc migration

--- market-depth-api/cryptoexchange-market-depth/Migrations/20241220141428_MiscChanges.Designer.cs
+++ market-depth-api/cryptoexchange-market-depth/Migrations/20241220141428_MiscChanges.Designer.cs
@@ -4,6 +4,7 @@ using CryptoexchangeMarketDepth.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CryptoexchangeMarketDepth.Migrations
 {
     [DbContext(typeof(OrderBookDbContext))]
-    partial class OrderBookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241220141428_MiscChanges")]
+    partial class MiscChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ market-depth-api/cryptoexchange-market-depth/Migrations/20241220141428_MiscChanges.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoexchangeMarketDepth.Migrations
+{
+    /// <inheritdoc />
+    public partial class MiscChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MarketSymbol",
+                table: "Snapshots",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MarketSymbol",
+                table: "Snapshots",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
